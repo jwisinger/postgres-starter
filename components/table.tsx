dd9e79ce084706e44a575dd705f9732525a8a4ce
jwisinger <54415083+jwisinger@users.noreply.github.com>
2026-01-19 19:26:58 -0600

    Initial customization

--- components/table.tsx
+++ components/table.tsx
@@ -11,31 +11,20 @@ export default async function Table() {
   let startTime = Date.now()
 
   try {
-    data = await sql`SELECT * FROM profiles`
+    data = await sql`SELECT * FROM "raceTable"`
   } catch (e: any) {
-    if (e.message.includes('relation "profiles" does not exist')) {
-      console.log(
-        'Table does not exist, creating and seeding it with dummy data now...'
-      )
-      // Table is not created yet
-      await seed()
-      startTime = Date.now()
-      data = await sql`SELECT * FROM profiles`
-    } else {
       throw e
-    }
   }
 
   const profiles = data
   const duration = Date.now() - startTime
-
+  
   return (
     <div className="bg-white/30 p-12 shadow-xl ring-1 ring-gray-900/5 rounded-lg backdrop-blur-lg max-w-xl mx-auto w-full">
       <div className="flex justify-between items-center mb-4">
         <div className="space-y-1">
-          <h2 className="text-xl font-semibold">Recent Users</h2>
+          <h2 className="text-xl font-semibold">Racers</h2>
           <p className="text-sm text-gray-500">
-            Fetched {profiles.length} users in {duration}ms
           </p>
         </div>
         <RefreshButton />
@@ -43,23 +32,23 @@ export default async function Table() {
       <div className="divide-y divide-gray-900/5">
         {profiles.map((user) => (
           <div
-            key={user.name}
+            key={user.Number}
             className="flex items-center justify-between py-3"
           >
-            <div className="flex items-center space-x-4">
-              <Image
-                src={user.image}
-                alt={user.name}
-                width={48}
-                height={48}
+            <div className="flex items-center space-x-4" >
+            <Image
+                src={`data:image/jpeg;base64,${Buffer.from(user.Image).toString('base64')}`}
+                alt={user.Name}
+                width={64}
+                height={64}
                 className="rounded-full ring-1 ring-gray-900/5"
-              />
+             />
               <div className="space-y-1">
-                <p className="font-medium leading-none">{user.name}</p>
-                <p className="text-sm text-gray-500">{user.email}</p>
+                <p className="font-medium leading-none">{user.Name}</p>
+                <p className="text-sm text-gray-500">Troop {user.Troop}</p>
               </div>
             </div>
-            <p className="text-sm text-gray-500">{timeAgo(user.createdAt)}</p>
+            <p className="text-sm text-gray-500">{user.Level}</p>
           </div>
         ))}
       </div>
